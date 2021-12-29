a963a0aba73483ba0c6d5387daae38b5e34c3657
Dan Wright <wrightdj99@gmail.com>
2021-12-29 13:20:07 -0600

    Adding external JARS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import java.util.*;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to Mailchamp!");
     }
 }
