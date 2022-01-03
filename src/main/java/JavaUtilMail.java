09cb191cfdf40b143c8f07e01b73c2637297bd72
Dan Wright <wrightdj99@gmail.com>
2022-01-03 12:17:27 -0600

    Now you can send a message about anything to any valid email!

--- src/main/java/JavaUtilMail.java
+++ src/main/java/JavaUtilMail.java
@@ -12,37 +12,43 @@ public class JavaUtilMail {
         System.out.println("What is the subject of the email you want to send?");
         String subject = scanner.nextLine();
         System.out.println("What is the message that you want to send?");
-        System.out.println("Preparing to send");
-        Properties properties = new Properties();
+        String messageText = scanner.nextLine();
+        if(messageText != null && subject != null) {
+            System.out.println("Preparing to send");
+            Properties properties = new Properties();
 
-        //mail.smtp.auth = deals with authentication for mail
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
+            //mail.smtp.auth = deals with authentication for mail
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.host", "smtp.gmail.com");
+            properties.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(properties, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                //return super.getPasswordAuthentication();
-                return new PasswordAuthentication(senderAccount, senderPassword);
-            }
-        });
+            Session session = Session.getInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    //return super.getPasswordAuthentication();
+                    return new PasswordAuthentication(senderAccount, senderPassword);
+                }
+            });
 
-        Message message = prepareMessage(session, senderAccount, receiver);
+            Message message = prepareMessage(session, senderAccount, receiver, subject, messageText);
 
-        Transport.send(message);
-        System.out.println("Message sent");
+            Transport.send(message);
+            System.out.println("Message sent");
+        }
+        else{
+            throw new Exception("Message subject and message must not be null");
+        }
     }
 
-    private static Message prepareMessage(Session session, String senderAccount, String receiver){
+    private static Message prepareMessage(Session session, String senderAccount, String receiver, String subject, String text){
         Message message = null;
         try {
             message = new MimeMessage(session);
             message.setFrom(new InternetAddress(senderAccount));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
-            message.setSubject("Testing message");
-            message.setText("Hi there, this is my first Java email");
+            message.setSubject(subject);
+            message.setText(text);
         }
         catch(Exception ex){
             Logger.getLogger(JavaUtilMail.class.getName()).log(Level.SEVERE, null, ex);
